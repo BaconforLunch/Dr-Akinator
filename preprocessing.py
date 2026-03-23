7b2536f0af7d2112d7d393b5d067bcbfd153a1ed
Ericsson Jake B. Camingao <jakecamingaosh@gmail.com>
2026-03-23 15:28:05 +0800

    initial code

--- preprocessing.py
+++ preprocessing.py
@@ -4,3 +4,23 @@ symptomSeverityFilepath = 'data/Symptom-severity.csv'
 datasetFilepath = 'data/dataset.csv'
 symptomDescriptionFilepath = 'data/symptom_Description.csv'
 symptomPrecautionFilepath = 'data/symptom_Precaution.csv'
+
+#filler for now
+def openFile(path:str) -> list[list[str]]|None:
+    try: 
+        file = open(path, "r")
+        res :list[list[str]]= []
+        
+        curLine = file.readline()
+        while curLine:
+            res.append([s.strip() for s in curLine.strip().split(',')])
+            curLine = file.readline()
+        file.close()
+        return res
+    except:
+        print("File error")
+
+#testing
+contents = openFile(symptomDescriptionFilepath)
+if contents:
+    for c in contents[0:2]: print(c)
