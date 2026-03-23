0e8ced0cca5da8338dad1c1b32074a099d85d240
Ericsson Jake B. Camingao <jakecamingaosh@gmail.com>
2026-03-23 15:11:32 +0800

    some init

---
+++ preprocessing.py
@@ -0,0 +1,6 @@
+import scipy
+
+symptomSeverityFilepath = 'data/Symptom-severity.csv'
+datasetFilepath = 'data/dataset.csv'
+symptomDescriptionFilepath = 'data/symptom_Description.csv'
+symptomPrecautionFilepath = 'data/symptom_Precaution.csv'
