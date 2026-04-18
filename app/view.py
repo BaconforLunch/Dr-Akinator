d277bbc3db80df084f42eb078291561e815f9796
Ericsson Jake B. Camingao <jakecamingaosh@gmail.com>
2026-04-19 00:13:05 +0800

    basic view

--- app/view.py
+++ app/view.py
@@ -1,3 +1,31 @@
 import streamlit as st
 st.title('Dr. Akinator')
 
+# initialize
+st.session_state.current_question = None
+
+def showQuestion():
+    if st.session_state.current_question is None:
+        st.text('No question yet (question here)')
+    else:
+        st.text(st.session_state.current_question)
+
+
+def answer(id:int):
+    print(id)
+
+
+showQuestion()
+
+c1, c2, c3, c4, c5 = st.columns([1,1,1,1,1])
+
+with c1:
+    if st.button('Yes', width='stretch'): answer(4)
+with c2:
+    if st.button('Probably', width='stretch'): answer(3)
+with c3:
+    if st.button('Maybe', width='stretch'): answer(2)
+with c4:
+    if st.button('Probably Not', width='stretch'): answer(1)
+with c5:
+    if st.button('No', width='stretch'): answer(0)
